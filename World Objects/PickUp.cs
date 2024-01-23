1e46469eb6c8f2c4a0f3358b612c6e3b6d4d7bb8
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-01-23 14:29:08 -0600

    On pick up item, instantiate floating score text

--- World Objects/PickUp.cs
+++ World Objects/PickUp.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour {
-	void OnTriggerEnter2D(Collider2D coll) {
+	[Header("Set in Inspector")]
+	public int value = 0;
+
+    void OnTriggerEnter2D(Collider2D coll) {
 		PlayerController player = coll.gameObject.GetComponent<PlayerController>();
 		if (player != null) {
 			// Deactivate gameObject
@@ -11,6 +14,9 @@ public class PickUp : MonoBehaviour {
 
 			// Play SFX
 			AudioManager.S.PlayPickUpSFX();
-		} 
+
+			// Instantiate floating score game object (temporarily displays this item's value to the user)
+            GameManager.S.InstantiateFloatingScore(PlayerController.S.gameObject, "+$" + value, Color.green);
+        } 
 	}
 }
