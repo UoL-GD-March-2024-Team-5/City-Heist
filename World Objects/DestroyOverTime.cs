084e558bb0a7fe5fcb18e558fe96a3815f01c228
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-01-23 13:45:12 -0600

    Add DestroyOverTime.cs
    Add DestroyOverTime.cs, which destroys or deactivates the attached game object after a specified amount of time.

---
+++ World Objects/DestroyOverTime.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Destroys or deactivates the attached game object after a specified amount of time.
+public class DestroyOverTime : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public bool destroyRatherThanSetActive;
+    public float timeDuration = 1f;
+
+    [Header("Set Dynamically")]
+    private float timeDone;
+
+    void OnEnable() {
+        timeDone = timeDuration + Time.time;
+
+        StartCoroutine("FixedUpdateCoroutine");
+    }
+
+    public IEnumerator FixedUpdateCoroutine() {
+        if (timeDone <= Time.time) {
+            if (destroyRatherThanSetActive) {
+                Destroy(gameObject);
+            } else {
+                gameObject.SetActive(false);
+            }
+        }
+
+        yield return new WaitForFixedUpdate();
+        StartCoroutine("FixedUpdateCoroutine");
+    }
+}
