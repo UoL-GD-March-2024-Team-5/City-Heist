32f9af605113c199cd75d45546cad6f4aebddb4e
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-01-23 13:45:59 -0600

    Add FloatingScore.cs
    Add FloatingScore.cs, which gradually increases the attached game object's y-position, and optionally, its scale.

---
+++ World Objects/FloatingScore.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Gradually increases the attached game object's y-position, and optionally, its scale.
+public class FloatingScore : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public float speed = 1f;
+
+    [Header("Set Dynamically")]
+    private bool canScale;
+
+    void OnEnable() {
+        StartCoroutine("FixedUpdateCoroutine");
+    }
+
+    void OnDisable() {
+        transform.localPosition = Vector3.zero;
+        transform.localScale = Vector3.one;
+    }
+
+    public IEnumerator FixedUpdateCoroutine() {
+        Invoke("ScaleUp", 1.0f);
+        if (canScale) {
+            transform.localScale += (Vector3.one * Time.fixedDeltaTime) / 3;
+        }
+
+        yield return new WaitForFixedUpdate();
+        StartCoroutine("FixedUpdateCoroutine");
+    }
+
+    void ScaleUp() {
+        canScale = true;
+    }
+}
