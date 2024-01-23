2444ee8b638ae2d595e8adaf7f3bcba759eede3c
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-01-22 21:23:47 -0600

    Add pause timer functions & activate timer object

--- Managers/GameManager.cs
+++ Managers/GameManager.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     [Header("Set in Inspector")]
     public string       firstScene;
+
+    public GameObject   timerGO;
+
+    public Button       goBackToLevelSelectButton;
 
     [Header("Set Dynamically")]
     // Pause
     public bool         paused;
-    public GameObject   pauseBlackScreen;
+    public GameObject   pauseMenu;
 
     // Singleton
     private static GameManager _S;
@@ -40,24 +41,66 @@ public class GameManager : MonoBehaviour
     void Start() {
         // Add Loop() to UpdateManager
         UpdateManager.updateDelegate += Loop;
+
+        // Add go back to level select button listener
+        goBackToLevelSelectButton.onClick.AddListener(delegate { GoBackToLevelSelectMenu(); });
+
+        // Deactivate timer game object
+        timerGO.SetActive(false);
     }
 
     public void Loop() {
         // Pause Input
-        if (!paused) {        
+        if (!paused) {
             if (Input.GetButtonDown("Pause")) {
-                paused = true;
-                Physics2D.gravity = Vector2.zero;
-                pauseBlackScreen.SetActive(true);
-
-                PlayerController.S.rigid.velocity = Vector2.zero;
+                PauseGame();
             }
         } else {
             if (Input.GetButtonDown("Pause") || Input.GetButtonDown("SNES B Button")) {
-                paused = false;
-                Physics2D.gravity = new Vector2(0, -15f);
-                pauseBlackScreen.SetActive(false);
-            }         
+                UnpauseGame();
+            }
         }
+    }
+
+    void PauseGame() {
+        // Pause timer
+        Timer.S.PauseTimer();
+
+        paused = true;
+        Physics2D.gravity = Vector2.zero;
+
+        // Activate pause menu
+        pauseMenu.SetActive(true);
+
+        // Set player velocity to 0
+        PlayerController.S.rigid.velocity = Vector2.zero;
+
+        // Set selected game object
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(goBackToLevelSelectButton.gameObject);
+    }
+
+    void UnpauseGame(bool unpauseTimer = true) {
+        // Unpause timer
+        if (unpauseTimer) {
+            Timer.S.UnpauseTimer();
+        }
+
+        paused = false;
+        Physics2D.gravity = new Vector2(0, -15f);
+
+        // Deactivate pause menu
+        pauseMenu.SetActive(false);
+    }
+
+    public void GoBackToLevelSelectMenu() {
+        // Deactivate timer game object
+        timerGO.SetActive(false);
+
+        // Load Scene
+        SceneManager.LoadScene("Level_Selection");
+
+        // Unpause game without unpausing the timer
+        UnpauseGame(false);
     }
 }
