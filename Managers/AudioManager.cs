9e68ecf151b6903ab9bf43f2250e509c005eba74
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-01-23 21:57:33 -0600

    Add PlaySFX: plays a specific SFX audio source
    Add PlaySFX, which plays a specific SFX audio source based on its input, an enum called 'eAudioClipName'.

--- Managers/AudioManager.cs
+++ Managers/AudioManager.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum eAudioClipName { buttonPressedSFX, pickUpSFX, springboardSFX, doorOpenSFX, dialogueSFX, dialogueEndSFX, unpauseSFX };
+
 public class AudioManager : MonoBehaviour {
     [Header("Set in Inspector")]
-    // Audio sources
+    // BGM audio source
     public AudioSource bgmAudioSource;
-    public AudioSource sfxAudioSource;
 
-    // Audio clips
+    // SFX audio sources
+    public AudioSource buttonPressedAudioSource;
+    public AudioSource dialogueAudioSource;
+    public AudioSource dialogueEndAudioSource;
+    public AudioSource doorOpenAudioSource;
+    public AudioSource pickUpAudioSource;
+    public AudioSource springboardAudioSource;
+    public AudioSource unpauseAudioSource;
+
+    // BGM audio clip
     public AudioClip bgm;
-    public AudioClip pickUpSFX;
-    public AudioClip springboardSFX;
 
     // Singleton
     private static AudioManager _S;
@@ -34,17 +42,34 @@ public class AudioManager : MonoBehaviour {
 
     void Start() {
         // Start playing background music
-        bgmAudioSource.clip = bgm;
-        bgmAudioSource.Play();
+        //bgmAudioSource.clip = bgm;
+        //bgmAudioSource.Play();
     }
 
-    public void PlayPickUpSFX() {
-        sfxAudioSource.clip = pickUpSFX;
-        sfxAudioSource.Play();
-    }
-
-    public void PlaySpringboardSFX() {
-        sfxAudioSource.clip = springboardSFX;
-        sfxAudioSource.Play();
+    // Plays a specific SFX audio source based on its input, an enum called 'eAudioClipName'
+    public void PlaySFX(eAudioClipName clipName) {
+        switch (clipName) {
+            case eAudioClipName.buttonPressedSFX:
+                buttonPressedAudioSource.Play();
+                break;
+            case eAudioClipName.dialogueSFX:
+                dialogueAudioSource.Play();
+                break;
+            case eAudioClipName.dialogueEndSFX:
+                dialogueEndAudioSource.Play();
+                break;
+            case eAudioClipName.doorOpenSFX:
+                doorOpenAudioSource.Play();
+                break;
+            case eAudioClipName.pickUpSFX:
+                pickUpAudioSource.Play();
+                break;
+            case eAudioClipName.springboardSFX:
+                springboardAudioSource.Play();
+                break;
+            case eAudioClipName.unpauseSFX:
+                unpauseAudioSource.Play();
+                break;
+        }
     }
 }
