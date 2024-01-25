ec8cc710e26878ecfd42a0e82a7b9bf56c5c56f7
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-01-25 16:25:31 -0600

    Add playSFXOnFirstTimeSelected bool
    Add playSFXOnFirstTimeSelected bool, which prevents playing 'selected' SFX for buttons that are dynamically selected on level load.

--- World Objects/OnSelectPlaySound.cs
+++ World Objects/OnSelectPlaySound.cs
@@ -7,10 +7,18 @@ using UnityEngine.EventSystems;
 // On this UI object selected, play sound
 public class OnSelectPlaySound : MonoBehaviour, ISelectHandler {
     [Header("Set in Inspector")]
-    public AudioSource audioSource;
+    public AudioSource  audioSource;
+
+    // Prevents playing 'selected' SFX for buttons that are dynamically selected on level load
+    public bool         playSFXOnFirstTimeSelected = true;
 
     public void OnSelect(BaseEventData eventData) {
-        audioSource.Play();
+        // Play SFX 
+        if (playSFXOnFirstTimeSelected) {
+            audioSource.Play();
+        } else {
+            playSFXOnFirstTimeSelected = true;
+        }
 
         // Activate Interactable Trigger
         InteractableCursor.S.Activate(gameObject.transform.position);
