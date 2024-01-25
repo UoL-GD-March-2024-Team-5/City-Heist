09d3735d45bcff26c866f228a7b5688a69181df6
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-01-25 16:51:22 -0600

    Add change script name
    Add change script name: replace ‘PickUp’ with ‘ItemTrigger’

---
+++ World Objects/ItemTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTrigger : MonoBehaviour {
+	[Header("Set in Inspector")]
+	public int value = 0;
+
+    void OnTriggerEnter2D(Collider2D coll) {
+		PlayerController player = coll.gameObject.GetComponent<PlayerController>();
+		if (player != null) {
+			// Deactivate gameObject
+			gameObject.SetActive(false);
+
+			// Play SFX
+			AudioManager.S.PlaySFX(eSFXAudioClipName.itemTriggerSFX);
+
+			// Instantiate floating score game object (temporarily displays this item's value to the user)
+            GameManager.S.InstantiateFloatingScore(PlayerController.S.gameObject, "+$" + value, Color.green);
+
+			// Increment score/total value of stolen items
+			ScoreManager.S.IncrementScore(value);
+        } 
+	}
+}
