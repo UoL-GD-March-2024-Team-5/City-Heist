ab50609eab213429fdf9b2a1f7f2bae7ae8ecd2f
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-01-25 14:31:55 -0600

    Remove unnecessary code used during testing

--- World Objects/LevelLoadTransition.cs
+++ World Objects/LevelLoadTransition.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 // On level load, close then open black “curtains” to hide level initialization from the user.
 public class LevelLoadTransition : MonoBehaviour {
-    [Header("Set in Inspector")]
-    public Animator anim;
-
     [Header("Set Dynamically")]
-    public bool isOpen;
+    public Animator anim;
 
     private static LevelLoadTransition _S;
     public static LevelLoadTransition S { get { return _S; } set { _S = value; } }
@@ -21,40 +18,18 @@ public class LevelLoadTransition : MonoBehaviour {
         anim = GetComponent<Animator>();
     }
 
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Z)) {
-            if(isOpen) {
-                Close();
-            } else {
-                Open();
-            }
-        }
-    }
-
     public void Open() {
-        Debug.Log("OPEN");
-
-        isOpen = true;
-
         if (Random.value > 0.5f) {
-            //anim.Play("HorizontalOpen");
             anim.CrossFade("HorizontalOpen", 0);
         } else {
-            //anim.Play("VerticalOpen");
             anim.CrossFade("VerticalOpen", 0);
         }
     }
 
     public void Close() {
-        Debug.Log("CLOSE");
-
-        isOpen = false;
-
         if (Random.value > 0.5f) {
-            //anim.Play("HorizontalClose");
             anim.CrossFade("HorizontalClose", 0);
         } else {
-            //anim.Play("VerticalClose");
             anim.CrossFade("VerticalClose", 0);
         }
     }
