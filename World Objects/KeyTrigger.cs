313ef4dc81521176972ae7cacdd40f3c8b75c03a
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-01-26 20:16:32 -0600

    On key pick up, play short jingle then resume BGM

--- World Objects/KeyTrigger.cs
+++ World Objects/KeyTrigger.cs
@@ -7,11 +7,20 @@ public class KeyTrigger : MonoBehaviour {
     [Header("Set in Inspector")]
     public List<string> message = new List<string>() { "Hey, you found a key...", "...now you can unlock a door!", };
 
+    [Header("Set Dynamically")]
+    private SpriteRenderer sRend;
+    private CircleCollider2D circleColl;
+
+    private void Start() {
+        sRend = GetComponent<SpriteRenderer>();
+        circleColl = GetComponent<CircleCollider2D>();
+    }
+
     void OnTriggerEnter2D(Collider2D coll) {
         PlayerController player = coll.gameObject.GetComponent<PlayerController>();
         if (player != null) {
-            // Deactivate gameObject
-            gameObject.SetActive(false);
+            // Play short celebratory jingle, then resume playing previous played BGM
+            StartCoroutine(AudioManager.S.PlayShortJingleThenResumePreviousBGM(4));
 
             // Set gravity to 0
             Physics2D.gravity = Vector2.zero;
@@ -19,11 +28,21 @@ public class KeyTrigger : MonoBehaviour {
             // Increment key count by one
             KeyManager.S.IncrementKeyCount(1);
 
-            // Play SFX
-            AudioManager.S.PlaySFX(eSFXAudioClipName.itemTriggerSFX);
-
             // Display text
             DialogueManager.S.DisplayText(message);
+
+            // Deactivate sprite renderer & trigger
+            sRend.enabled = false;
+            circleColl.enabled = false;
+
+            // Deactivate this game object in approximately 4 seconds (the delay
+            // is needed, otherwise coroutine would be stopped prematurely)
+            Invoke("DeactivateThisObject", 4f);
         }
+    }
+
+    // Deactivate gameObject
+    void DeactivateThisObject() {
+        gameObject.SetActive(false);
     }
 }
