7633e19e1baae86de4807efa5a377c3664647f2e
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-01-28 21:24:10 -0600

    Add DistractionTrigger.cs
    Add DistractionTrigger.cs, which on button press, activate this object, a TV or radio, to lure people and pets away.

---
+++ World Objects/DistractionTrigger.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// On button press, activate this object, a TV or radio, to lure people and pets away
+public class DistractionTrigger : MonoBehaviour {
+    [Header("Set Dynamically")]
+    public Animator anim;
+
+    public bool     isTurnedOn;
+
+    public bool     playerIsInTrigger;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Update() {
+        if (playerIsInTrigger) {
+            if (Input.GetKeyDown(KeyCode.E)) {
+                // Swap animation  & play SFX
+                if (isTurnedOn) {
+                    anim.CrossFade("Off", 0);
+                    isTurnedOn = false;
+
+                    AudioManager.S.PlaySFX(eSFXAudioClipName.unpauseSFX);
+                } else {
+                    anim.CrossFade("On", 0);
+                    isTurnedOn = true;
+
+                    AudioManager.S.PlaySFX(eSFXAudioClipName.buttonSelectedSFX);
+                }
+            }
+        }
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.gameObject.tag == "PlayerTrigger") {
+            playerIsInTrigger = true;
+
+            // Activate Interactable Trigger
+            InteractableCursor.S.Activate(gameObject);
+        }
+    }
+
+    protected virtual void OnTriggerExit2D(Collider2D coll) {
+        if (coll.gameObject.tag == "PlayerTrigger") {
+            playerIsInTrigger = false;
+
+            // Deactivate Interactable Trigger
+            InteractableCursor.S.Deactivate();
+        }
+    }
+}
