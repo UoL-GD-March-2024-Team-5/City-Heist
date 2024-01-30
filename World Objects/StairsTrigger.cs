615a4380fc8fdfd4af32ac740173d2c313464a6c
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-01-29 21:44:04 -0600

    Add StairsTrigger.cs
    Add StairsTrigger.cs, which on button press, sets the player's position to the opposite end (top or bottom) of this staircase.

---
+++ World Objects/StairsTrigger.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// On button press, sets the player's position to the opposite end (top or bottom) of this staircase
+public class StairsTrigger : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public Transform    destinationTransform;
+    public Animator     directionArrowAnim;
+    public bool         isGoingUpstairs;
+    
+    [Header("Set Dynamically")]
+    public bool         playerIsInTrigger;
+
+    public void Update() {
+        if (playerIsInTrigger) {
+            if (isGoingUpstairs) {
+                if (Input.GetAxisRaw("Vertical") > 0f) {
+                    SetPlayerPosToOppositeEndOfStairs();
+                }
+            } else {
+                if (Input.GetAxisRaw("Vertical") < 0f) {
+                    SetPlayerPosToOppositeEndOfStairs();
+                }
+            }
+        }
+    }
+
+    // Relocate player position to the opposite end of this staircase
+    void SetPlayerPosToOppositeEndOfStairs() {
+        // Set player to destination position 
+        PlayerController.S.gameObject.transform.position = destinationTransform.position;
+
+        // Play SFX
+        AudioManager.S.PlaySFX(eSFXAudioClipName.stairsSFX);
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.gameObject.tag == "PlayerTrigger") {
+            playerIsInTrigger = true;
+
+            // Play direction arrow on animation clip
+            directionArrowAnim.CrossFade("On", 0);
+        }
+    }
+
+    protected virtual void OnTriggerExit2D(Collider2D coll) {
+        if (coll.gameObject.tag == "PlayerTrigger") {
+            playerIsInTrigger = false;
+
+            // Play direction arrow off animation clip
+            directionArrowAnim.CrossFade("Off", 0);
+        }
+    }
+}
