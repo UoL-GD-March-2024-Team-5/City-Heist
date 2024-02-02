507b076b603f051a45002081eff6fc1f1fcc48b3
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-02-01 18:49:21 -0600

    Add RoomDarknessTrigger.cs
    Add RoomDarknessTrigger.cs, which on player trigger enter, if lights are turned off in this room, deactivate all NPC vision cones in scene.

---
+++ World Objects/RoomDarknessTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// On player trigger enter, if lights are turned off in this room, deactivate all NPC vision cones in scene
+public class RoomDarknessTrigger : MonoBehaviour {
+    protected virtual void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.gameObject.tag == "PlayerTrigger") {
+            // Dectivate all vision cones in current scene
+            GameManager.S.ActivateVisionCones(false);
+        }
+    }
+
+    protected virtual void OnTriggerExit2D(Collider2D coll) {
+        if (coll.gameObject.tag == "PlayerTrigger") {
+            // Activate all vision cones in current scene
+            GameManager.S.ActivateVisionCones(true);
+        }
+    }
+}
