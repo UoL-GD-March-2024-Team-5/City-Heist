141f3f8af472bdf6c9fc3f441ba317b293b616fc
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-02-05 17:59:38 -0600

    Add Utilities.cs
    Add Utilities.cs, which is a set of general helper functions to be used across the entire project.

---
+++ Managers/Utilities.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// A set of general helper functions to be used across the entire project.
+public class Utilities : MonoBehaviour {
+    [Header("Set Dynamically")]
+    private static Utilities _S;
+    public static Utilities S { get { return _S; } set { _S = value; } }
+
+    void Awake() {
+        S = this;
+    }
+}
