8c7c90fdcba4ae80dea515952136ca7f3c946eaf
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-02-05 18:01:59 -0600

    Add function to explicitly set button navigation

--- Managers/Utilities.cs
+++ Managers/Utilities.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // A set of general helper functions to be used across the entire project.
 public class Utilities : MonoBehaviour {
@@ -10,5 +11,23 @@ public class Utilities : MonoBehaviour {
 
     void Awake() {
         S = this;
+    }
+
+    // Explicitly set a button's navigation
+    public void SetButtonNavigation(Button button, Button buttonSelectOnUp = null, Button buttonSelectOnDown = null, Button buttonSelectOnLeft = null, Button buttonSelectOnRight = null) {
+        // Get the Navigation data
+        Navigation navigation = button.navigation;
+
+        // Switch mode to Explicit to allow for custom assigned behavior
+        navigation.mode = Navigation.Mode.Explicit;
+
+        // Select buttons to navigate to on directional input
+        navigation.selectOnUp = buttonSelectOnUp;
+        navigation.selectOnDown = buttonSelectOnDown;
+        navigation.selectOnLeft = buttonSelectOnLeft;
+        navigation.selectOnRight = buttonSelectOnRight;
+
+        // Reassign the struct data to the button
+        button.navigation = navigation;
     }
 }
