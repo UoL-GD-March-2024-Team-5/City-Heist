f9145cc92579a420dbb0d9adafc6ca0b5c5a6331
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-02-07 16:03:30 -0600

    Add lockpick door mechanic
    On player trigger enter and button pressed, start 5 second timer. With each passing second, the amount of seconds left is instantiated above the door. After 5 seconds, the door is automatically unlocked and opened. On player trigger exit before countdown finished, the countdown is canceled.

--- World Objects/DoorTrigger.cs
+++ World Objects/DoorTrigger.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,9 +12,9 @@ public class DoorTrigger : MonoBehaviour {
     public bool             doorOpensToLeft;
 
     public string           doorIsLockedMessage = "This door is locked. Find a key!";
-    public string           doorIsUnlockedMessage = "Great, you unlocked the door. Now open it!";
+    public string           doorIsUnlockedMessage = "Great, you unlocked the door!";
 
-    public Sprite           closedDoorSprite, openDoorLeftSprite, openDoorRightSprite;
+    public Sprite           closedDoorSprite, openDoorLeftSprite, openDoorRightSprite, lockedDoorSprite;
 
     public EdgeCollider2D   solidColl;
     public BoxCollider2D    triggerColl;
@@ -22,8 +24,15 @@ public class DoorTrigger : MonoBehaviour {
 
     public bool             playerIsInTrigger;
 
+    int                     secondsLeftToLockpick = 5;
+
     void Start() {
         sRend = GetComponent<SpriteRenderer>();
+
+        // Set initial sprite to render
+        if (doorIsLocked) {
+            sRend.sprite = lockedDoorSprite;
+        }
     }
 
     public void Update() {
@@ -36,7 +45,10 @@ public class DoorTrigger : MonoBehaviour {
                         if (KeyManager.S.keyCount > 0) {
                             UnlockAndOpenDoor();
                         } else {
-                            DoorIsLocked();
+                            // Reset seconds left to lockpick door
+                            secondsLeftToLockpick = 5;
+
+                            StartCoroutine("AttemptToLockpickDoor", secondsLeftToLockpick);
                         }
 
                         // Prevents calling DisplayText() above repeatedly on button press,
@@ -83,12 +95,56 @@ public class DoorTrigger : MonoBehaviour {
         OpenDoor();
     }
 
+    // Unused after '5 seconds to lockpick' mechanic introduced
     void DoorIsLocked() {
         // Display text
         DialogueManager.S.DisplayText(doorIsLockedMessage);
 
         // Play SFX
         AudioManager.S.PlaySFX(eSFXAudioClipName.unpauseSFX);
+    }
+
+    // Countdown from 5 seconds until the player has lockpicked this door
+    public IEnumerator AttemptToLockpickDoor(int secondsLeft) {
+        // Instantiate a floating number that displays the amount of seconds left until the player's lockpicked this door
+        InstantiateSecondsLeftToLockpickFloatingNumber(secondsLeft);
+
+        // Decrement seconds left until door unlocked
+        secondsLeft -= 1;
+
+        // Wait for 1 second
+        yield return new WaitForSecondsRealtime(1);
+
+        if (secondsLeft > 0) {
+            // Play SFX
+            AudioManager.S.PlaySFX(eSFXAudioClipName.dialogueEndSFX);
+
+            // Loop this coroutine
+            StartCoroutine("AttemptToLockpickDoor", secondsLeft);
+        } else {
+            // Play SFX
+            AudioManager.S.PlaySFX(eSFXAudioClipName.pauseAudioSource);
+
+            // Finally lockpick and open door
+            UnlockAndOpenDoor();
+        }
+    }
+
+    // Instantiate a floating number that displays the amount of seconds left until the player's lockpicked this door
+    void InstantiateSecondsLeftToLockpickFloatingNumber(int secondsLeft) {
+        switch (secondsLeft) {
+            case 5:
+            case 4:
+                GameManager.S.InstantiateFloatingScore(PlayerController.S.gameObject, secondsLeft.ToString(), Color.red);
+                break;
+            case 3:
+            case 2:
+                GameManager.S.InstantiateFloatingScore(PlayerController.S.gameObject, secondsLeft.ToString(), Color.yellow);
+                break;
+            case 1:
+                GameManager.S.InstantiateFloatingScore(PlayerController.S.gameObject, secondsLeft.ToString(), Color.green);
+                break;
+        }
     }
 
     protected virtual void OnTriggerEnter2D(Collider2D coll) {
@@ -106,6 +162,8 @@ public class DoorTrigger : MonoBehaviour {
 
             // Deactivate Interactable Trigger
             InteractableCursor.S.Deactivate();
+
+            StopCoroutine("AttemptToLockpickDoor");
         }
     }
 }
