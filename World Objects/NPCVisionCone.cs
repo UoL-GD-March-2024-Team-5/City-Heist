22bad66e53dda77e07dfdd5125045c9bbc1ca041
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-02-14 15:58:11 -0600

    On player detected, activate “!” word bubble

--- World Objects/NPCVisionCone.cs
+++ World Objects/NPCVisionCone.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 // On player trigger enter, the burglar has been caught and the game is over.
 public class NPCVisionCone : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public GameObject wordBubbleGO;
+
     protected virtual void OnTriggerEnter2D(Collider2D coll) {
         if (coll.gameObject.tag == "PlayerTrigger") {
+            // Activate "!" word bubble
+            wordBubbleGO.SetActive(true);
+
             // The player has been seen by an NPC, game over!
             GameManager.S.levelEndManagerCS.GameOver();
         }
